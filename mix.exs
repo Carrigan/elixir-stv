4213c46f2389836a8c51eaa0d7ece1b85bee766f
Brian Carrigan <brian.carrigan@smashingboxes.com>
2016-05-26 07:20:34 -0400

    Corrected app name.

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Stv.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :elixir_stv,
+    [app: :stv,
      version: "0.0.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
